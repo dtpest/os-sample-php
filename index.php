ba28eca80499684bbf624605ad89d26b11b4ce27
Peter Studener <peter.studener@dynatrace.com>
2017-05-02 08:52:03 +0200

    env vars

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-  echo "<h1 style='text-align:center'>Peter</h1><pre>";
-  var_dump($_ENV);
-  phpinfo();
+$connStr = "host=" . $_ENV['POSTGRESQL_PORT_5432_TCP_ADDR'] . " port=5432 dbname=postgres user=postgres options='--application_name=$appName'";
+echo $connStr . "<br />";
+echo "<h1 style='text-align:center'>Peter</h1><pre>";
+var_dump($_ENV);
+phpinfo();
 ?>
